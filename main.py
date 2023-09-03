4d0c6a829288bad7d985439227caf91b0568c59f
Chris Kim <chriskk@unc.edu>
2023-09-03 18:08:54 -0400

    Added date-selection functionality

--- main.py
+++ main.py
@@ -2,33 +2,71 @@ from selenium.webdriver import Edge
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
+from datetime import datetime, timedelta
 
-driver = Edge()
-driver.get("https://calendar.lib.unc.edu/reserve/davis-study-rooms")
 
-time.sleep(2)
+def main():
+    global driver 
+    driver = Edge()
+    driver.get("https://calendar.lib.unc.edu/reserve/davis-study-rooms")
 
-date = driver.find_element(By.CSS_SELECTOR, "button.fc-goToNextAvailable-button")
-date.click()
+    # Get the current date
+    current_date = datetime.now().date()
 
-time.sleep(2)
+    # Calculate the date one week ahead
+    two_weeks_ahead = current_date + timedelta(weeks=2)
 
-element = driver.find_element(By.CSS_SELECTOR, "div.fc-timeline-event-harness a.fc-timeline-event")
-element.click()
+    # Extract the month and date from the calculated date, fill in left side
+    # with 0 if it is a single digit number
+    month = f"{two_weeks_ahead.month:02d}"
+    day = f"{two_weeks_ahead.day:02d}"
 
-time.sleep(2)
+    select_date(month, day)
+    reserve(month, day)
+    driver.quit
 
-drop_down = driver.find_element(By.CSS_SELECTOR, "select#bookingend_1")
-time.sleep(2)
-option = Select(drop_down)
-time.sleep(2)
-option.select_by_value("2023-08-30 09:30:00")
-time.sleep(2)
 
-button_element = driver.find_element(By.CSS_SELECTOR, "button#submit_times")
-button_element.click()
-time.sleep(5)
+def reserve(month: int, day: int):
 
-print("Success")
+    time.sleep(2)
 
-driver.quit
+    element = driver.find_element(By.CSS_SELECTOR, "div.fc-timeline-event-harness a.fc-timeline-event")
+    element.click()
+
+    time.sleep(2)
+
+    drop_down = driver.find_element(By.CSS_SELECTOR, "select#bookingend_1")
+    time.sleep(2)
+    option = Select(drop_down)
+    time.sleep(2)
+    option.select_by_value(f"2023-{month}-{day} 12:30:00")
+    time.sleep(2)
+
+    button_element = driver.find_element(By.CSS_SELECTOR, "button#submit_times")
+    button_element.click()
+    time.sleep(5)
+
+
+def select_date(month, day):
+    # Locate the "Go To Date" button and click it
+    go_to_date_button = driver.find_element(By.CSS_SELECTOR, '.fc-goToDate-button')
+    go_to_date_button.click()
+
+    # Click the month switch to open the month selection menu
+    month_switch = driver.find_element(By.CSS_SELECTOR, '.datepicker-switch')
+    month_switch.click()
+
+    # Select the desired month
+    months = driver.find_elements(By.CSS_SELECTOR, '.month')
+    months[int(month) - 1].click()
+
+    # Select the desired day
+    days = driver.find_elements(By.CSS_SELECTOR, '.day')
+    for d in days:
+        if d.text == str(day):
+            d.click()
+            break
+    time.sleep(5)
+
+if __name__ == "__main__":
+    main()
