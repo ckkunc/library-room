593e754bd6da1a6608dc5bea1dec7a3b1953dfb9
Chris Kim <chriskk@unc.edu>
2023-09-14 03:08:50 -0400

    Added registration functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Program to automatically reserve rooms in Davis Library.
 
 id="bookingend_1" changes to "bookingend_58406917" if other rooms are reserved.
+
+'eid_29084', '8052'
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from selenium.webdriver import Edge
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 import time
 from datetime import datetime, timedelta
 
@@ -28,58 +29,80 @@ def main():
     string_month = two_weeks_ahead.strftime('%B')
     time.sleep(2)
     select_date(month, day)
-    #reserve(month, day, year)
     time.sleep(1)
 
+    # If the room is already booked at 3:30, try the next room above it
     for row_id, room_num in zip(row_id, room_num):
-        try:
-            select_time_slot(row_id, room_num, f'{string_day}, {string_month} {day}, {year}', '3:00pm', month, day, year)
+        success = select_time_slot(row_id, room_num, f'{string_day}, {string_month} {day}, {year}', '3:30pm', month, day, year)
+        if success:
             break  # If the function call was successful, break the loop
-        except NoSuchElementException:
-            continue  # If a NoSuchElementException was raised, continue with the next iteration
+        
     input_box()
     driver.quit
 
 
 def input_box():
-    onyen = 'onyen'
-    pw = 'pw'
+    onyen = 'chriskk'
+    pw = 'CKim10611520!'
     username_box = driver.find_element(By.CSS_SELECTOR, '#username')
     username_box.send_keys(onyen)
     time.sleep(1)
     next_button = driver.find_element(By.CSS_SELECTOR, '#nextBtn')
     next_button.click()
+    time.sleep(3)
     password_box = driver.find_element(By.CSS_SELECTOR, '#password')
     password_box.send_keys(pw)
-
-
-def select_time_slot(row, room, date, time_slot, month, day, year):
-    # Locate the row for the desired room
-    room_row = driver.find_element(By.XPATH, f"//td[@class='fc-timeline-lane fc-resource' and @data-resource-id='{row}']")
-
-    # Locate the time slot within the row
-    time_slot = room_row.find_element(By.XPATH, f".//a[@class='fc-timeline-event fc-h-event fc-event fc-event-start fc-event-end fc-event-future s-lc-eq-avail' and @title='{time_slot} {date} - Room {room} - Available']")
-
-    # Click on the time slot to select it
-    time_slot.click()
-
+    submit_button = driver.find_element(By.CSS_SELECTOR, '#submitBtn')
+    submit_button.click()
     time.sleep(3)
-    
-    # If the dropdown menu's ID isn't 58406914, try finding the menu with ID 1
+    complete_booking = driver.find_element(By.CSS_SELECTOR, '#terms_accept')
+    complete_booking.click()
+    time.sleep(3)
+    input_box = driver.find_element(By.CSS_SELECTOR, '#nick')
+    input_box.send_keys('Studying')
+    submit_booking = driver.find_element(By.CSS_SELECTOR, '#btn-form-submit')
+    submit_booking.click()
+    time.sleep(3)
+
+
+def select_time_slot(row, room, date, time_slot, month, day, year) -> bool:
     try:
-        drop_down = driver.find_element(By.CSS_SELECTOR, 'select#bookingend_58406917')
+        # Locate the row for the desired room
+        room_row = driver.find_element(By.XPATH, f"//td[@class='fc-timeline-lane fc-resource' and @data-resource-id='{row}']")
+
+        # Locate the time slot within the row
+        time_slot = room_row.find_element(By.XPATH, f".//a[@class='fc-timeline-event fc-h-event fc-event fc-event-start fc-event-end fc-event-future s-lc-eq-avail' and @title='{time_slot} {date} - Room {room} - Available']")
+
+        # Click on the time slot to select it
+        time_slot.click()
+
+        time.sleep(2)
+        
+        # If the dropdown menu's ID isn't 58406914, try finding the menu with ID 1
+        try:
+            drop_down = driver.find_element(By.CSS_SELECTOR, 'select#bookingend_58406917')
+        except NoSuchElementException:
+            drop_down = driver.find_element(By.CSS_SELECTOR, 'select#bookingend_1')
+        time.sleep(1)   
+        option = Select(drop_down)
+        time.sleep(1)
+
+        times = ['18:30:00', '18:00:00', '1:730:00', '17:00:00', '16:30:00', '16:00:00',]
+        for slot in times:
+            try:
+                option.select_by_value(f'{year}-{month}-{day} {slot}')
+                break
+            except NoSuchElementException:
+                continue
+                
+        time.sleep(2)
+
+        button_element = driver.find_element(By.CSS_SELECTOR, 'button#submit_times')
+        button_element.click()
+        time.sleep(2)
+        return True
     except NoSuchElementException:
-        drop_down = driver.find_element(By.CSS_SELECTOR, 'select#bookingend_1')
-    time.sleep(1)
-    option = Select(drop_down)
-    time.sleep(1)
-    option.select_by_value(f'{year}-{month}-{day} 17:30:00')
-    time.sleep(2)
-
-    button_element = driver.find_element(By.CSS_SELECTOR, 'button#submit_times')
-    button_element.click()
-    time.sleep(2)
-
+        return False
 
 def select_date(month, day):
     # Locate the 'Go To Date' button and click it
@@ -95,7 +118,7 @@ def select_date(month, day):
     months[int(month) - 1].click()
 
     # Select the desired day
-    days = driver.find_elements(By.CSS_SELECTOR, '.day')
+    days = driver.find_elements(By.CSS_SELECTOR, 'td.day:not(.old):not(.disabled)')
     for d in days:
         if d.text == str(day):
             d.click()
